a99385fa16fbbb68670ada60fef78cb3d552fcc9
RhysRoo <rhys.bundy97@gmail.com>
2025-08-30 13:06:47 +0100

    Initial Commit

---
+++ CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by Rhys Parsons on 30/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
